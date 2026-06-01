8b8ed71c4d47dd819506b2908f0f1dc5e9091e24
Graham Asher <graham@cartotype.com>
2026-06-01 13:07:10 +0100

    updated shell script to run the Linux Maps App: it didn't work on Lubuntu

--- src/apps/Maps/CartoTypeMaps.sh
+++ src/apps/Maps/CartoTypeMaps.sh
@@ -1,19 +1,8 @@
-#!/bin/sh
+##!/bin/sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+export LD_LIBRARY_PATH="$SCRIPT_DIR:$LD_LIBRARY_PATH"
 
-# This shell script sets the shared library path and runs CartoTypeMaps: see http://doc.qt.io/qt-5/linux-deployment.html
+# Point to the root plugin folder instead of just platforms
+export QT_PLUGIN_PATH="$SCRIPT_DIR/plugins"
 
-appname=`basename $0 | sed s,\.sh$,,`
-
-dirname=`dirname $0`
-tmp="${dirname#?}"
-
-if [ "${dirname%$tmp}" != "/" ]; then
-dirname=$PWD/$dirname
-fi
-
-# For the moment (since upgrading to Qt 15.1 on Ubuntu) setting the library path is disabled, because it prevented the OpenGL context from being created,
-# giving the error "QXcbIntegration: Cannot create platform OpenGL context, neither GLX nor EGL are enabled"
-# LD_LIBRARY_PATH=$dirname
-# export LD_LIBRARY_PATH
-
-$dirname/$appname "$@"
+exec "$SCRIPT_DIR/CartoTypeMaps" "$@"
